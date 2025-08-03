8d780a3b7f2e6f17c00a9a50ef587ae9ee83ba45
egbog <rysimpry@yahoo.com>
2025-08-03 03:29:07 -0700

    simplify + exceptions

--- Client/Utils/ParseProfile.cs
+++ Client/Utils/ParseProfile.cs
@@ -1,62 +1,66 @@
-﻿using BepInEx.Logging;
+﻿using System;
+using System.Collections.Generic;
+using EFT;
 using EFT.InventoryLogic;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
-namespace _RepairMaxDurability.Utils {
-    public class ParseProfile {
-        private readonly string          idSophie, idBenji;
-        private readonly int             newD,     maxD, repR;
-        private readonly ManualLogSource log;
+namespace _RepairMaxDurability.Utils;
 
-        public ParseProfile(JObject json) {
-            log = Logger.CreateLogSource("MaxDura");
-
-            // item repaired
-            JToken sophie = json?.SelectToken("Items").First;
-            // repair kit
-            JToken benji = sophie?.Next;
-
-            if (sophie != null && benji != null) {
-                idSophie = (string)sophie.SelectToken("_id");
-                idBenji  = (string)benji.SelectToken("_id");
-                newD     = (int)sophie.SelectToken("upd.Repairable.Durability");
-                maxD     = (int)sophie.SelectToken("upd.Repairable.MaxDurability");
-                repR     = (int)benji.SelectToken("upd.RepairKit.Resource");
+public class ParseProfile {
+    public record Profile {
+        public record Item {
+            public record Repairable {
+                public int Durability    { get; set; }
+                public int MaxDurability { get; set; }
             }
+
+            public record RepairKit {
+                public int Resource { get; set; }
+            }
+
+            public record Upd {
+                public Repairable Repairable { get; set; }
+                public RepairKit  RepairKit  { get; set; }
+            }
+
+            public MongoID _id { get; set; }
+            public Upd     upd { get; set; }
         }
 
-        public bool UpdateValues(RepairableComponent targetItemRC, Item repairKit) {
-            // set weapon durability
-            if (targetItemRC.Item.Id == idSophie) {
-                targetItemRC.Durability    = newD;
-                targetItemRC.MaxDurability = maxD;
-                targetItemRC.Item.UpdateAttributes();
-                //this.log.LogInfo(item.LocalizedName() + " REPAIRED TO: " + repairableComponent.MaxDurability);
+        [JsonProperty("data")]
+        public List<Item> Items { get; set; }
+    }
+
+    public static void UpdateValues(Profile profile, RepairableComponent targetItemRc, Item repairKit) {
+        if (profile.Items.Find(i => i._id == targetItemRc.Item.Id) is { } item) {
+            targetItemRc.Durability    = item.upd.Repairable.Durability;
+            targetItemRc.MaxDurability = item.upd.Repairable.MaxDurability;
+            targetItemRc.Item.UpdateAttributes();
+            //this.Log.LogInfo(item.LocalizedName() + " REPAIRED TO: " + repairableComponent.MaxDurability);
+        }
+        else // something went wrong with json sent from server
+        {
+            throw new Exception($"Target item {targetItemRc.Item.Id} not found in server.");
+        }
+
+        // update repair kit resource
+        if (profile.Items.Find(i => i._id == repairKit.Id) is { } kit) {
+            repairKit.TryGetItemComponent(out RepairKitComponent repairKitComponent);
+            repairKitComponent.Resource = kit.upd.RepairKit.Resource;
+            repairKit.RaiseRefreshEvent(true);
+            //Plugin.Log.LogDebug("NEW REPAIR RESOURCE: " + rkc.Resource);
+
+            // delete repair kit at 0 resource or below
+            if (repairKitComponent.Resource <= 0) {
+                var traderControllerClass = (TraderControllerClass)repairKit.Parent.GetOwner();
+                traderControllerClass.ThrowItem(repairKit);
+                //Plugin.Log.LogDebug("DESTROYED REPAIR KIT");
             }
-            else // something went wrong with json sent from server
-            {
-                return false;
-            }
 
-            // update repair kit resource
-            if (repairKit.Id == idBenji) {
-                RepairKitComponent repairKitComponent = repairKit.GetItemComponent<RepairKitComponent>();
-                repairKitComponent.Resource = repR;
-                repairKit.RaiseRefreshEvent(true);
-                //this.log.LogInfo("NEW REPAIR RESOURCE: " + rkc.Resource);
-
-                // delete repair kit at 0 resource or below
-                if (repairKitComponent.Resource <= 0) {
-                    var traderControllerClass = (TraderControllerClass)repairKit.Parent.GetOwner();
-                    traderControllerClass.ThrowItem(repairKit);
-                    //this.log.LogInfo("DESTROYED REPAIR KIT");
-                }
-
-                return true; // all is well - minister fudge
-            }
-
-            // here too
-            return false;
+            // all is well - minister fudge
+        }
+        else {
+            throw new Exception($"Target item {repairKit.Id} not found in server.");
         }
     }
 }

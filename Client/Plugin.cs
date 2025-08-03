bc893a55e247ac7979eb29603499b4265f68f86d
egbog <rysimpry@yahoo.com>
2025-08-03 03:28:08 -0700

    move logger

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -7,12 +7,16 @@
 
 using _RepairMaxDurability.Patches;
 using BepInEx;
+using BepInEx.Logging;
 
 namespace _RepairMaxDurability {
+
     [BepInPlugin("com.egbog.maxdura", "MaxDurability", "2.0.0")]
     [BepInProcess("EscapeFromTarkov.exe")]
     public class Plugin : BaseUnityPlugin
     {
+        public static readonly ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("RepairMaxDurability");
+
         private void Awake()
         {
             // Plugin startup logic

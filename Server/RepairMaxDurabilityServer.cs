ceaa2be17582a3614fa2aefe85b88802c943fc2e
egbog <3422424+egbog@users.noreply.github.com>
2025-10-11 14:59:35 -0700

    update version range

--- Server/RepairMaxDurabilityServer.cs
+++ Server/RepairMaxDurabilityServer.cs
@@ -17,7 +17,7 @@ public record ModMetadata : AbstractModMetadata {
     public override string                                          Author            { get; init; } = "egbog";
     public override List<string>?                                   Contributors      { get; init; }
     public override SemanticVersioning.Version                      Version           { get; init; } = new("0.0.1"); 
-    public override SemanticVersioning.Range                        SptVersion        { get; init; } = new("4.0.0");
+    public override SemanticVersioning.Range                        SptVersion        { get; init; } = new("~4.0.0");
     public override List<string>?                                   Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>?   ModDependencies   { get; init; }
     public override string?                                         Url               { get; init; }  = "https://github.com/egbog/Repair-Max-Durability";

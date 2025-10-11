d240decb6d94e0eea1c4c91968685512351eff83
egbog <3422424+egbog@users.noreply.github.com>
2025-10-11 14:57:41 -0700

    update modmetadata

--- Server/RepairMaxDurabilityServer.cs
+++ Server/RepairMaxDurabilityServer.cs
@@ -17,9 +17,9 @@ public record ModMetadata : AbstractModMetadata {
     public override string                                          Author            { get; init; } = "egbog";
     public override List<string>?                                   Contributors      { get; init; }
     public override SemanticVersioning.Version                      Version           { get; init; } = new("0.0.1"); 
-    public override SemanticVersioning.Version                      SptVersion        { get; init; } = new("4.0.0");
+    public override SemanticVersioning.Range                        SptVersion        { get; init; } = new("4.0.0");
     public override List<string>?                                   Incompatibilities { get; init; }
-    public override Dictionary<string, SemanticVersioning.Version>? ModDependencies   { get; init; } = new();
+    public override Dictionary<string, SemanticVersioning.Range>?   ModDependencies   { get; init; }
     public override string?                                         Url               { get; init; }  = "https://github.com/egbog/Repair-Max-Durability";
     public override bool?                                           IsBundleMod       { get; init; }  = false;
     public override string                                          License           { get; init; } = "MIT";
